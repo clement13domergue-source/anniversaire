68a64f34cdc4de6753ee4a682021611d64f3e830
clement13domergue-source <clement13.domergue@orange.fr>
2026-04-20 19:13:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const slides = [
     text: "Il y a des gens qui arrivent dans ta vie et qui la changent pour toujours. Toi, tu en fais partie."
   },
   {
-    image: "images/image2.jpg.PNG",
+    image: "images/image2.jpg.jpg",
     text: "Ce sourire-là… impossible de l'oublier. Même quand tu fais semblant de ne pas être fier(e) de toi. 😄"
   },
   {
